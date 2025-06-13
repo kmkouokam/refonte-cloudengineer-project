e0cfca3105c77a31d4d9d87857626cc358659048
klover22 <klovversolutions@hotmail.com>
2025-06-13 14:27:55 -0400

    modules

---
+++ modules/bastion/main.tf
@@ -0,0 +1,53 @@
+
+data "aws_ssm_parameter" "ubuntu_ami" {
+  name = "/aws/service/canonical/ubuntu/server/24.04/stable/current/amd64/hvm/ebs-gp3/ami-id"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "image-id"
+    values = [data.aws_ssm_parameter.ubuntu_ami.value]
+  }
+}
+
+
+resource "aws_instance" "bastion" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.bastion_instance_type
+  subnet_id              = var.public_subnet_id
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  key_name               = var.key_name
+
+  associate_public_ip_address = true
+
+  tags = merge(var.tags, {
+    Name = "${var.env}-bastion"
+  })
+}
+
+resource "aws_security_group" "bastion_sg" {
+  name        = "${var.env}-bastion-sg"
+  description = "Allow SSH access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_ssh_cidrs
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.tags, {
+    Name = "${var.env}-bastion-sg"
+  })
+}
